cd23328f63db75733f75af78966ddc45606196af
Adam Kohl <adamkohl@iastate.edu>
2015-09-15 23:09:20 -0500

    Problem 1 document and code

---
+++ Homework_2/p1/p1/build/p1.build/Debug/p1.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/adamkohl/Documents/Graduate_School/ME557/ak/Homework_2/p1/p1/p1/main.cpp
---
+++ Homework_2/p1/p1/p1/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  p1
+//
+//  Created by Adam Kohl on 9/15/15.
+//  Copyright (c) 2015 Adam Kohl. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+double myFunction(int n )
+{
+    unsigned int i = 0, j = 1, t, k;
+    for (k=0; k <= n; ++k)
+    {
+        t = i + j;
+        i = j;
+        j = t;
+    }
+    return j;
+}
+
+int main(int argc , char *argv[])
+{
+    double p = myFunction(10);
+    cout << p << endl;
+}
